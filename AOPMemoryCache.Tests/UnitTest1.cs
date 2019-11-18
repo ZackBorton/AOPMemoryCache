2f26193e2e078f81ed5fe1461f768693e1f55e85
Zack Borton <zaborton@gmail.com>
2019-11-17 22:52:57 -0600

    Initial commit

---
+++ AOPMemoryCache.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AOPMemoryCache.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ AOPMemoryCache/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AOPMemoryCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO : Integrate with MemCache Example class and show example useage           
+        }
+    }
+}
